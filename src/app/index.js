86bdd29f5e14c34f20bd1d9bab5b2e5e0956d2fb
Chase Sang <chase.sang@gmail.com>
2017-06-21 23:01:28 -0700

    first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,17 @@
-alert('it works');
+var React = require ('react');
+var ReactDOM = require('react-dom');
+
+// Create component
+var TodoComponent = React.createClass({
+
+
+  render: function(){
+    return(
+      <h1> Helloooo </h1>
+    );
+  }
+
+});
+
+// Put component into html page
+ReactDOM.render(<TodoComponent />, document.getElementById("todo-wrapper"));
