c21de6fc999b55fce62b236ccbc7925b20545045
Chase Sang <chase.sang@gmail.com>
2017-06-21 23:50:04 -0700

    add set state

--- src/app/index.js
+++ src/app/index.js
@@ -6,13 +6,20 @@ var TodoComponent = React.createClass({
   getInitialState: function(){
     return{
       todos:['washup', 'eat', 'gym'],
+      age:30
     }
   },
 
   render: function(){
+    var ager = setTimeout(function(){
+      this.setState({
+        age: 35
+      });
+    }.bind(this),5000);
     return(
       <div id="todo-list">
       <p> The busiest people have the most leisure... </p>
+      <p> {this.state.age} </p>
       <ul>
         <li> {this.state.todos[0]}</li>
         <li> {this.state.todos[1]}</li>
