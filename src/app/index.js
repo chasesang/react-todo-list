eda1c399bbb46c09cfbffa3d1fce02f519a2e175
Chase Sang <chase.sang@gmail.com>
2017-06-28 20:02:29 -0700

    fix routeing

--- src/app/index.js
+++ src/app/index.js
@@ -1,70 +1,68 @@
-var React = require ('react');
+var React = require('react');
 var ReactDOM = require('react-dom');
+import { Router, Route, browserHistory, Link } from 'react-router';
 
-// Create component
-var TodoComponent = React.createClass({
-  getInitialState: function(){
-    return{
-      todos:['washup', 'eat', 'gym','eat'],
-      age:30
+//Module requires
+var TodoItem = require('./todoItem');
+var AddItem = require('./addItem');
+var About = require('./about');
+
+//CSS requires
+require('./css/index.css');
+
+
+//SETUP ROUTING
+var App = React.createClass({
+    render: function(){
+        return(
+            <Router history={browserHistory}>
+                <Route path={"/"} component={TodoComponent}></Route>
+                <Route path={"/about"} component={About}></Route>
+            </Router>
+        );
     }
-  },
-
-  render: function(){
-
-    var todos = this.state.todos;
-    todos = todos.map(function(item,index){
-      return(
-        // nested components
-        <TodoItem item={item} key={index} />
-      );
-    });
-
-
-    var ager = setTimeout(function(){
-      this.setState({
-        age: 35
-      });
-    }.bind(this),5000);
-
-    return(
-      <div id="todo-list">
-      <p onClick={this.clicked}> The busiest people have the most leisure... </p>
-      <p> {this.state.age} </p>
-      <ul>
-        {todos}
-      </ul>
-      </div>
-    );
-  }, // render
-
-
-  // custom fucntions
-  clicked: function(){
-    console.log('you clicked me!');
-  }
 });
 
-// create TodoItem component
-var TodoItem = React.createClass({
-  render: function(){
-    return(
-      <li>
-        <div className = "todo-item">
-          <span className="item-name">{this.props.item}</span>
-          <span className='item-delete' onClick={this.handleDelete}> x </span>
-        </div>
-      </li>
-    );
-  },
-  // custom functions
-  handleDelete:function(){
+//Create a component
+var TodoComponent = React.createClass({
+    getInitialState: function(){
+        return {
+            todos: ['wash up', 'eat some cheese', 'take a nap']
+        }
+    }, //getInitialState
+    render: function(){
+        var todos = this.state.todos;
+        todos = todos.map(function(item, index){
+            return(<TodoItem key={index} item={item} onDelete={this.onDelete} />);
+        }.bind(this));
+        return(
+            <div id="todo-list">
+                <Link to={"/about"}>About</Link>
+                <p>The busiest people have the most leisure...</p>
+                <ul>{todos}</ul>
+                <AddItem onAdd={this.onAdd} />
+            </div>
+        );
+    }, //render
 
-  }
+    //Custom functions
+    onDelete: function(item){
+        var updatedTodos = this.state.todos.filter(function(val, index){
+            return item !== val;
+        });
+        this.setState({
+          todos: updatedTodos
+        });
+    },
+
+    onAdd: function(item){
+        var updatedTodos = this.state.todos;
+        updatedTodos.push(item);
+        this.setState({
+            todos: updatedTodos
+        })
+    }
+
 });
 
-var myCheese = {name: 'Camemebert', smellFactor: 'Extreme pong', price: '3.50'};
-
-
-// Put component into html page
-ReactDOM.render(<TodoComponent mssg="I like cheese" cheese ={myCheese} />, document.getElementById("todo-wrapper"));
+ReactDOM.render(<App />, document.getElementById('todo-wrapper'));
